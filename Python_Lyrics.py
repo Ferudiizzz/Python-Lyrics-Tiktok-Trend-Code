1ac26a2945a647b147b8cb24193b6df137d00242
Ferudi <ferdinand11jr@gmail.com>
2024-11-18 00:11:27 +0800

    Create Python_Lyrics.py

---
+++ Python_Lyrics.py
@@ -0,0 +1,41 @@
+import time
+
+
+
+def write_lyrics():
+    lyrics = [
+
+
+        "Garyeojin oraen sigani",
+        "Urireul dasi bulleowa",
+        "Eoneu gose isseodo",
+        "Geu kkeuteun hangsang neoingeol",
+        "",
+        "'Cause I'm falling slowly love with you",
+        "Oraetdongan gidaryeoon",
+        "Neoneun bomiya",
+        "'Cause I'm falling slowly love with you",
+        "Dasi jiwojinda haedo",
+        "All my life is you",
+        "",
+        "Neoraneun iyagi soge",
+        "Dasi tto kkumeul kkuneun na",
+        "Eotteon sungani wado",
+        "Nan neoreul chaja galgeoya",
+        "",
+        "'Cause I'm falling slowly love with you",
+        "Oraetdongan gidaryeoon",
+        "Neoneun bomiya",
+        "'Cause I'm falling slowly love with you",
+        "Dasi jiwojinda haedo",
+        "All my life is you"
+
+
+        
+    ]
+
+    for line in lyrics:
+        print(line)
+        time.sleep(1)  # Delay of 1 second between each line
+
+write_lyrics()
